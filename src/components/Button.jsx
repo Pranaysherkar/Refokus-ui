f45df2caf0f9ce74994f8f3f33eb776aabcc232f
Pranay Sherkar <116248469+Pranaysherkar@users.noreply.github.com>
2024-03-10 17:14:38 +0530

    button animation set

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,16 +4,16 @@ import { BsArrowReturnRight } from "react-icons/bs";
 function Button({ title = "Start a Project" }) {
 const[ishover,setHover]=useState(false);
   return (
-    <button onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)} className="w-40 h-10 bg-white text-zinc-600 text-base rounded-full overflow-hidden">
-      <motion.div whileHover={{marginTop:"-40px"}} className="flex items-center justify-between px-4 py-2">
+    <div className="w-40 h-10 bg-white text-zinc-600 text-base rounded-full overflow-hidden">
+      <div onMouseEnter={()=>setHover(true)} onMouseLeave={()=>setHover(false)} className={`flex items-center justify-between px-4 py-2 duration-500 ${ishover ? `-mt-10` : ``}`}>
     {title}
       <BsArrowReturnRight className="text-black text-xs" />
-    </motion.div>
-    <motion.div className="flex items-center justify-between px-4 py-2 ">
+    </div>
+    <div className="flex items-center justify-between px-4 py-2 ">
     {title}
       <BsArrowReturnRight className="text-black text-xs" />
-    </motion.div>
-    </button>
+    </div>
+    </div>
   );
 }
 
